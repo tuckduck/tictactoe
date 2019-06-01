9d90adcc996c9d0711fddf0796119c63c1337aaa
tuckduck <tucker.reinhardt12@gmail.com>
2019-06-01 17:28:59 -0500

    medium computer implemented using board_analysis class to perform win/block moves and replay loop in main

--- src/tictactoe/board_analysis.java
+++ src/tictactoe/board_analysis.java
@@ -1,13 +1,15 @@
 package tictactoe;
 
-public class board_analysis {
+import java.util.HashSet;
+
+class board_analysis {
   private int[] win_vector_player1;
   private int[] win_vector_player2;
-  public board_analysis(){
+  board_analysis(){
     this.win_vector_player1 = new int[8];
     this.win_vector_player2 = new int[8];
   }
-  public boolean check_win() {
+  boolean check_win() {
     for(int i =0;i < 8; i++) {
       if (win_vector_player1[i] == 3 || win_vector_player2[i] == 3) {
         return true;
@@ -15,7 +17,52 @@ public class board_analysis {
     }
     return false;
   }
-  public void incoming_move(int move, char person) {
+  HashSet<Integer> get_priority_ones(char identifier){
+    HashSet<Integer> priorities = new HashSet<>();
+    int[] vector = (identifier == 'X') ? this.win_vector_player1: this.win_vector_player2;
+    if(vector[0] == 2){
+      priorities.add(0);
+      priorities.add(1);
+      priorities.add(2);
+    }
+    if(vector[1] == 2){
+      priorities.add(3);
+      priorities.add(4);
+      priorities.add(5);
+    }
+    if(vector[2] == 2){
+      priorities.add(6);
+      priorities.add(7);
+      priorities.add(8);
+    }
+    if(vector[3] == 2){
+      priorities.add(0);
+      priorities.add(3);
+      priorities.add(6);
+    }
+    if(vector[4] == 2){
+      priorities.add(1);
+      priorities.add(4);
+      priorities.add(7);
+    }
+    if(vector[5] == 2){
+      priorities.add(2);
+      priorities.add(5);
+      priorities.add(8);
+    }
+    if(vector[6] == 2){
+      priorities.add(0);
+      priorities.add(4);
+      priorities.add(8);
+    }
+    if(vector[7] == 2){
+      priorities.add(2);
+      priorities.add(4);
+      priorities.add(6);
+    }
+    return priorities;
+  }
+  void incoming_move(int move, char person) {
     int[] vector = (person =='X') ? this.win_vector_player1 : this.win_vector_player2;
     switch(move){
       case 0:
--- src/tictactoe/game.java
+++ src/tictactoe/game.java
@@ -4,26 +4,35 @@ import java.util.*;
 public class game {
   public static void main(String args[]){
     System.out.println("Hello welcome to Tic-Tac-Toe");
-    char[] ref_board = {'0','1','2','3','4','5','6','7','8'};
     Scanner player = new Scanner(System.in);
-    System.out.println("What game do you want to play? 1 - human vs. human  |  2 - human vs. easy computer");
-    int gamemode = get_player_int(player);
-    int winner;
-    boolean computer = false;
-    switch(gamemode) {
-      case 1:
-        winner = two_player_game(ref_board, player);
+    while(true) {
+      char[] ref_board = {'0', '1', '2', '3', '4', '5', '6', '7', '8'};
+      System.out.println(
+          "What game do you want to play? 1 - human vs. human  |  2 - human vs. easy computer  |  3 - human vs. medium computer");
+      int gamemode = get_player_int(player);
+      int winner;
+      boolean computer = false;
+      switch (gamemode) {
+        case 1:
+          winner = two_player_game(ref_board, player);
+          break;
+        case 2:
+          winner = easy_Computer(ref_board, player);
+          computer = true;
+          break;
+        case 3:
+          winner = medium_Computer(ref_board, player);
+          break;
+        default:
+          winner = 3;
+      }
+      String winning_message = (winner == 3) ? "Tie game" : (winner == 1) ? (computer) ? "You Win!" : "Player X Wins!"
+                                                                : (computer) ? "You Lose!" : "Player O Wins!";
+      System.out.println(winning_message);
+      System.out.println("Play Again? 0 - Yes  |  1 - No");
+      if(get_player_int(player) != 0)
         break;
-      case 2:
-        winner = easy_Computer(ref_board, player);
-        computer = true;
-        break;
-      default:
-        winner = 3;
     }
-    String winning_message = (winner == 3) ? "Tie game" : (winner == 1)?(computer)?"You Win!":"Player X Wins!"
-                                                              :(computer)? "You Lose!":"Player O Wins!";
-    System.out.println(winning_message);
   }
   private static int two_player_game(char[] board, Scanner player){
     board_analysis referee = new board_analysis();
@@ -88,7 +97,54 @@ public class game {
         return 2;
     }
     return 3;
-
+  }
+  private static int medium_Computer(char[] board, Scanner player) {
+    ArrayList<Integer> available= new ArrayList<>(java.util.Arrays.asList(0,1,2,3,4,5,6,7,8));
+    board_analysis referee = new board_analysis();
+    Random randy = new Random();
+    print_board(board);
+    int move;
+    while(!available.isEmpty()) {
+      move = get_player_move(available, 'X', player);
+      available.remove(Integer.valueOf(move));
+      referee.incoming_move(move, 'X');
+      board[move] = 'X';
+      print_board(board);
+      if(referee.check_win())
+        return 1;
+      if(!win_or_block(board, referee,available))
+        rand_Computer_move(board, randy,available,referee);
+      print_board(board);
+      if(referee.check_win())
+        return 2;
+    }
+    return 3;
+  }
+  private static boolean win_or_block(char[] board, board_analysis referee, ArrayList<Integer> available) {
+    HashSet<Integer> possibilities = referee.get_priority_ones('O');
+    int move;
+    for(Integer possibility:possibilities) {
+      if(available.contains(possibility)){
+        move = possibility;
+        board[move] = 'O';
+        referee.incoming_move(move, 'O');
+        System.out.println("The computer chose spot " + move);
+        available.remove(Integer.valueOf(move));
+        return true;
+      }
+    }
+    possibilities = referee.get_priority_ones('X');
+    for(Integer possibility:possibilities) {
+      if(available.contains(possibility)){
+        move = possibility;
+        board[move] = 'O';
+        referee.incoming_move(move, 'O');
+        System.out.println("The computer chose spot " + move);
+        available.remove(Integer.valueOf(move));
+        return true;
+      }
+    }
+    return false;
   }
   private static void rand_Computer_move(char[] board, Random randy, ArrayList<Integer> available,board_analysis referee) {
     int randy_int = -1;
@@ -114,4 +170,3 @@ public class game {
     }
   }
 }
-
